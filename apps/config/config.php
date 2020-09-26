16f01478c8de77cbdc0d720f0574cc1f22d268e7
nersus15 <38497343+nersus15@users.noreply.github.com>
2020-09-26 11:51:05 +0800

    Update config.php

--- apps/config/config.php
+++ apps/config/config.php
@@ -1,15 +1,15 @@
 <?php
-define('BASEURL', 'https://hipelmas.kamscode.tech/');
-define('STATIC_PATH', "https://hipelmas.kamscode.tech/public/");
+define('BASEURL', 'https://domain.com/');
+define('STATIC_PATH', "https://domain.com/public/");
 define("VENDOR_PATH", "https://cdn.kamscode.tech/");
 define('APP_PATH', str_replace('config', '', __DIR__));
 define('PROJECT_PATH', str_replace('apps/', '', APP_PATH));
-define('DB_HOST', 'kamscode.tech');
-define('DB_USER', 'kamscode');
-define('DB_PASS', '3bS9Fn2g8n');
-define('DB_NAME', 'kamscode_hipelmas');
-define('controller_def', 'home');
-define('method_def', 'index');
+define('DB_HOST', 'host database');
+define('DB_USER', 'username db');
+define('DB_PASS', 'password db');
+define('DB_NAME', 'database name');
+define('controller_def', 'default controller');
+define('method_def', 'default method');
 define('IS_ROUTE', false);
 define('DEPENDENCIES_PATH', str_replace('apps/', '', APP_PATH) . 'vendor/');
 date_default_timezone_set('Asia/Singapore');
