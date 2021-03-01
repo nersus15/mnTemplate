72b2d2686058c71c9321cfe0d9c7983cb7da2ea8
nersus15 <38497343+nersus15@users.noreply.github.com>
2021-03-01 15:40:08 +0800

    Update config.php

--- apps/config/config.php
+++ apps/config/config.php
@@ -6,7 +6,7 @@ define('STATIC_PATH', "https://localhost/mnframework/public/");
 define("VENDOR_PATH", "https://cdn.kamscode.tech/");
 define('APP_PATH', str_replace('config', '', __DIR__));
 define('PROJECT_PATH', str_replace('apps/', '', APP_PATH));
-define('ASSETS_PATH', str_replace('apps/config', '', APP_PATH) . 'public/asset/');
+define('ASSETS_PATH', str_replace('apps', '', APP_PATH) . 'public/asset/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
